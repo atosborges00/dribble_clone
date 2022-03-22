db966670fc77bbaba5430d89b47c7febe40f5c66
atosborges00 <atosborges00@gmail.com>
2022-03-22 15:11:16 -0300

    feat: creating Shots model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  resources :shots
   devise_for :users, controller: { registrations: 'registrations' }
-  root "home#index"
+  root "shots#index"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_22_175910) do
+ActiveRecord::Schema.define(version: 2022_03_22_180307) do
+
+  create_table "shots", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
